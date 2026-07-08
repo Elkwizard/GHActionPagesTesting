2ac399865a753e8a02aa0b3dd753cf03923e5ee7
Elkwizard <bananamanthefourth@gmail.com>
2026-07-08 13:44:49 -0700

    added static file generator

---
+++ generate.js
@@ -0,0 +1,13 @@
+import fs from "node:fs";
+
+const json = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+fs.writeFileSync("./Generated/data.html", `
+	<!DOCTYPE html>
+	<head>
+		<title>${json.header}</title>
+	</head>
+	<body>
+		<h1>${json.header}</h1>
+		<p><i>${json.italic}</i></p>
+	</body>
+`, "utf-8");
